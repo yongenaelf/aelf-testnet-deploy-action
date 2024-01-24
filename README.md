bdc03ed83a90a9fc6163d76a5cba0d2d841b7aea
Loong Yong En <yongen.loong@aelf.io>
2024-01-24 10:23:49 +0800

    feat: v1

---
+++ README.md
@@ -0,0 +1,39 @@
+# AElf Testnet Deploy javascript action
+
+This action deploys a given compiled aelf contract to the aelf testnet.
+
+## Inputs
+
+### `private-key`
+
+**Required** Private key generated with aelf-command.
+
+### `wallet-address`
+
+**Required** AElf Address of the wallet associated with the private key.
+
+### `dll-filename`
+
+**Required** Path to the dll.patched file. Default `"src/bin/Debug/net6.0/HelloWorld.dll.patched"`.
+
+### `node-url`
+
+**Required** AElf Blockchain node url. Default `"https://tdvw-test-node.aelf.io"`.
+
+## Outputs
+
+### `block-hash`
+
+Block Hash used to check for deployed contract address.
+
+## Example usage
+
+1. Using [aelf-command](https://docs.aelf.io/en/latest/reference/cli/methods.html), generate a new wallet.
+2. Using [Portkey](https://portkey.finance/), get some testnet tokens at https://testnet-faucet.aelf.io/, transfer to the sidechain of the wallet in (1).
+
+```yaml
+uses: yongenaelf/aelf-testnet-deploy-action@HASH
+with:
+  private-key: "PRIVATE_KEY"
+  wallet-address: "WALLET_ADDRESS"
+```
---
+++ index.js
@@ -0,0 +1,73 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const AElf = require("aelf-sdk");
+const BigNumber = require("bignumber.js");
+const fs = require("fs");
+
+(async () => {
+  try {
+    const PRIVATE_KEY = core.getInput("private-key");
+    const WALLET_ADDRESS = core.getInput("wallet-address");
+    const DLL_FILENAME = core.getInput("dll-filename");
+    const NODE_URL = core.getInput("node-url");
+
+    const aelf = new AElf(new AElf.providers.HttpProvider(NODE_URL));
+
+    const wallet = AElf.wallet.getWalletByPrivateKey(PRIVATE_KEY);
+
+    const tokenContractName = "AElf.ContractNames.Token";
+    let tokenContractAddress;
+
+    // get chain status
+    const chainStatus = await aelf.chain.getChainStatus();
+    // get genesis contract address
+    const GenesisContractAddress = chainStatus.GenesisContractAddress;
+    // get genesis contract instance
+    const zeroContract = await aelf.chain.contractAt(
+      GenesisContractAddress,
+      wallet
+    );
+
+    // Get contract address by the read only method `GetContractAddressByName` of genesis contract
+    tokenContractAddress = await zeroContract.GetContractAddressByName.call(
+      AElf.utils.sha256(tokenContractName)
+    );
+
+    const tokenContract = await aelf.chain.contractAt(
+      tokenContractAddress,
+      wallet
+    );
+
+    const { balance } = await tokenContract.GetBalance.call({
+      symbol: "ELF",
+      owner: WALLET_ADDRESS,
+    });
+
+    if (balance === "0") {
+      throw new Error("Please claim your tokens from faucet");
+    }
+
+    const _balance = new BigNumber(balance).dividedBy(10 ** 8).toNumber();
+
+    console.log(`You have ${_balance} ELF.`);
+
+    const fileStr = fs.readFileSync(DLL_FILENAME).toString("base64");
+
+    const currentBlockHeight = await aelf.chain.getBlockHeight();
+    console.log(currentBlockHeight, "--currentBlockHeight");
+
+    const { BlockHash: currentBlockHash } = await aelf.chain.getBlockByHeight(
+      currentBlockHeight,
+      false
+    );
+    console.log(currentBlockHash, "--currentBlockHash");
+    core.setOutput("block-hash", currentBlockHash);
+
+    await zeroContract.DeployUserSmartContract({
+      category: 0,
+      code: fileStr,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+})();
